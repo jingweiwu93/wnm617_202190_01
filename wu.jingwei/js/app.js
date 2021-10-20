959a0dc639dc916e6fa17cf83746be8fd0cc7c5f
Jingwei Wu <jingweiwu93@gmail.com>
2021-10-20 14:14:15 -0700

    midterm

--- wu.jingwei/js/app.js
+++ wu.jingwei/js/app.js
@@ -35,20 +35,36 @@ $(()=>{
 
 
 	.on("click","[data-activate]",function(e){
-		e.preventDefault();
 		let target = $(this).data("activate");
 		$(target).addClass("active");
 	})
 	.on("click","[data-deactivate]",function(e){
-		e.preventDefault();
 		let target = $(this).data("deactivate");
 		$(target).removeClass("active");
 	})
 	.on("click","[data-toggle]",function(e){
-		e.preventDefault();
 		let target = $(this).data("toggle");
 		$(target).toggleClass("active");
 	})
+	.on("click","[data-activateone]",function(e){
+		let target = $(this).data("activateone");
+		console.log(target)
+		$(target).addClass("active").siblings().removeClass('active');
+	});
+
+
+	$("[data-template]").each(function(){
+		let target = $(this).data("template");
+		$(this).html($(target).html());
+	})
+
+
+	$({
+		"#page-recent":".nav-icon-set li:nth-child(1)",
+		"#page-list":".nav-icon-set li:nth-child(2)",
+		"#page-user-profile":".nav-icon-set li:nth-child(3)",
+	}[location.hash]).addClass("active");
+
 
 
 });
