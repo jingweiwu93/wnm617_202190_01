c6eaadaef5d3da3c1abea4c980f1cfb7799088bd
Jingwei Wu <jingweiwu93@gmail.com>
2021-11-10 01:49:17 -0800

    mod 9 update

--- wu.jingwei/js/pages.js
+++ wu.jingwei/js/pages.js
@@ -7,9 +7,6 @@ const ListPage = async() => {
 		console.log(error);
 		return;
 	}
-
-	console.log(result,error);
-
 	$("page-list .animallist").html(makeAnimallist(result));
 }
 
@@ -23,8 +20,8 @@ const UserProfilePage = async() => {
 	let {result,error} = await query({type:'user_by_id',params:[sessionStorage.userId]});
 
 	if(error) {
-	console.log(error);
-	return;
+		console.log(error);
+		return;
 	}
 	let [user] = result;
 	$("#page-user-profile [data-role='main']").html(makeUserProfile(user));
