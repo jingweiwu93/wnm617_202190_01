c873c38e11889640eef2da175f6510b4f133106c
Jingwei Wu <jingweiwu93@gmail.com>
2021-11-23 21:09:22 -0800

    Module 11

--- wu.jingwei/data/api.php
+++ wu.jingwei/data/api.php
@@ -45,34 +45,55 @@ function makeQuery($c,$ps,$p,$makeResults=true) {
    }
 }
 
-function makeStatement($data){
+function makeStatement($data) {
    try{
       $c = makeConn();
-      $t = $data->type;
-      $p = $data->params;
+      $t = @$data->type;
+      $p = @$data->params;
 
       switch($t) {
-         case "users_all":
-            return makeQuery($c,"SELECT * FROM `track_users`",$p);
-         case "animals_all":
-            return makeQuery($c,"SELECT * FROM `track_animals`",$p);
-         case "locations_all":
-            return makeQuery($c,"SELECT * FROM `track_locations`",$p);
+         // case "users_all":
+         //    return makeQuery($c,"SELECT * FROM `track_users`",$p);
+         // case "animals_all":
+         //    return makeQuery($c,"SELECT * FROM `track_animals`",$p);
+         // case "locations_all":
+         //    return makeQuery($c,"SELECT * FROM `track_locations`",$p);
 
          case "user_by_id":
-            return makeQuery($c,"SELECT * FROM `track_users` WHERE `id`=?",$p);
-         case "animals_by_id":
+            return makeQuery($c,"SELECT id,username,name,email,img FROM `track_users` WHERE `id`=?",$p);
+         case "animal_by_id":
             return makeQuery($c,"SELECT * FROM `track_animals` WHERE `id`=?",$p);
          case "location_by_id":
-                  return makeQuery($c,"SELECT * FROM `track_locations` WHERE `id`=?",$p);
+            return makeQuery($c,"SELECT * FROM `track_locations` WHERE `id`=?",$p);
 
          case "animals_by_user_id":
-             return makeQuery($c,"SELECT * FROM `track_animals` WHERE `user_id`=?",$p);
+            return makeQuery($c,"SELECT * FROM `track_animals` WHERE `user_id`=?",$p);
          case "locations_by_animal_id":
-             return makeQuery($c,"SELECT * FROM `track_locations` WHERE `animal_id`=?",$p);
+            return makeQuery($c,"SELECT * FROM `track_locations` WHERE `animal_id`=?",$p);
+
 
          case "check_signin":
             return makeQuery($c,"SELECT id FROM `track_users` WHERE `username`=? AND `password`=md5(?)",$p);
+
+         case "recent_animal_locations":
+            return makeQuery($c,"SELECT *
+               FROM `track_animals` a
+               JOIN (
+                  SELECT lg.*
+                  FROM `track_locations` lg
+                  WHERE lg.id = (
+                     SELECT lt.id
+                     FROM `track_locations` lt
+                     WHERE lt.animal_id = lg.animal_id
+                     ORDER BY lt.date_create DESC
+                     LIMIT 1
+                  )
+               ) l
+               ON a.id = l.animal_id
+               WHERE a.user_id = ?
+               ORDER BY l.animal_id, l.date_create DESC
+               ",$p);
+
 
          default: return ["error"=>"No Matched Type"];
       }
@@ -80,6 +101,7 @@ function makeStatement($data){
       return ["error"=>"Bad Data"];
    }
 }
+
 
 
 $data = json_decode(file_get_contents("php://input"));
